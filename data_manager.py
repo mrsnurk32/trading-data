48bf6fd5f40cb72e59a921bbf7a36801fcc4c8de
Nick <nickbelik@mail.ru>
2020-05-01 21:01:28 +0300

    New features: 1. Added ability to create custom path to store stock data. 2. Default path is current working folder path 3. Fixed error that caused error when files_inplace() executed.

--- data_manager.py
+++ data_manager.py
@@ -2,6 +2,8 @@ import pytz
 import MetaTrader5 as mt5
 from datetime import datetime
 import pandas as pd
+import sqlite3 as sql
+import os
 
 
 
@@ -9,7 +11,32 @@ import pandas as pd
 class DataManager:
 
     def __init__(self):
-        pass
+        self.storage_directory = os.getcwd().replace('\\','/')
+
+    def default_path(self):
+        print(os.getcwd())
+        self.storage_directory = os.getcwd().replace('\\','/')
+
+    def custom_path(self):
+        self.storage_directory = input('Enter path')
+
+    def files_inplace(self):
+        #This method tracks if files exist in directory mentioned in self.storage_directory
+        #If everything is fine returns True
+        arr = os.listdir(self.storage_directory)
+        #print(arr)
+        if 'stock_data' not in arr:
+            os.mkdir('{}/stock_data'.format(self.storage_directory))
+
+        if not os.path.exists(
+            '{}/stock_data/fin_data.db'.format(self.storage_directory)):
+            conn = sql.connect(
+                '{}/stock_data/fin_data.db'.format(self.storage_directory))
+            #conn.cursor()
+            conn.close()
+
+        return True
+
 
     def download_stock(self):
 
