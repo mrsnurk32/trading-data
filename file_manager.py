e5c24a6fe9d539cdff91a7507d4cff147f001b31
Nick <nickbelik@mail.ru>
2020-05-04 00:45:48 +0300

    Timezone added as parameter

--- file_manager.py
+++ file_manager.py
@@ -14,6 +14,7 @@ class FileManager:
 
     def __init__(self):
         self.storage_directory = os.getcwd().replace('\\','/')
+        self.timezone = pytz.timezone("Etc/UTC")
 
     def default_path(self):
         print(os.getcwd())
