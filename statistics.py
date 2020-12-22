8bb63e2e666b7e3d1be53e5cda4cb7ae544a1025
mrsnurk32 <nickbelik@mail.ru>
2020-12-22 20:55:43 +0300

    class DataManager moved to init

--- statistics.py
+++ statistics.py
@@ -2,25 +2,28 @@ from data_retriever import DataManager
 
 
 #return correlation table
-class Correlation_table(DataManager):
+class Correlation_table:
+
+    def __init__(self):
+        self.data_manager = DataManager()
 
     @staticmethod
     def combined_frame(tickers = None):
 
         if tickers is None:
-            tickers = DataManager().ticker_list[:10]
+            tickers = self.data_manager.ticker_list[:10]
 
 
         with concurrent.futures.ThreadPoolExecutor() as executor:
             
             counter = 0
             
-            frame = s.get_frame(tickers[0],time_frame = '1h',column_list = ['time','close'], rows = 5000)
+            frame = self.data_manager.get_frame(tickers[0],time_frame = '1h',column_list = ['time','close'], rows = 5000)
             frame[tickers[0]] = frame.close.pct_change()
 
             while counter < 9:
                 counter += 1
-                data = s.get_frame(tickers[counter],time_frame = '1h',column_list = ['time','close'], rows = 5000)
+                data = self.data_manager.get_frame(tickers[counter],time_frame = '1h',column_list = ['time','close'], rows = 5000)
                 frame[tickers[counter]] = data.close.pct_change()
         
             del frame['close']
