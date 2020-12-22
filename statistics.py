bd6606b64e4d59d58128875c12392d9efe257c13
mrsnurk32 <nickbelik@mail.ru>
2020-12-22 20:52:16 +0300

    Correlation_table class added

--- statistics.py
+++ statistics.py
@@ -1,7 +1,29 @@
-class DependencyTest:
+from data_retriever import DataManager
+
+
+#return correlation table
+class Correlation_table(DataManager):
 
     @staticmethod
-    def dependency_test(col_a, col_b):
-        pass
+    def combined_frame(tickers = None):
+
+        if tickers is None:
+            tickers = DataManager().ticker_list[:10]
+
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            
+            counter = 0
+            
+            frame = s.get_frame(tickers[0],time_frame = '1h',column_list = ['time','close'], rows = 5000)
+            frame[tickers[0]] = frame.close.pct_change()
+
+            while counter < 9:
+                counter += 1
+                data = s.get_frame(tickers[counter],time_frame = '1h',column_list = ['time','close'], rows = 5000)
+                frame[tickers[counter]] = data.close.pct_change()
+        
+            del frame['close']
+
 
 
